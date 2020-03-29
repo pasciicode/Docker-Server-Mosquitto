854a128f8062ea56a7e050e4aa223f29a2012abd
pasciicode <769830+pasciicode@users.noreply.github.com>
2020-03-29 20:41:01 +0200

    Update docker.cmd

--- docker.cmd
+++ docker.cmd
@@ -1,8 +1,8 @@
 docker run -itd --name=mosquitto \
 --restart=always \
--v /opt/mosquitto/config/mosquitto.conf:/mosquitto/config/mosquitto.conf:ro \
--v /opt/mosquitto/data:/mosquitto/data:rw \
--v /opt/mosquitto/log:/mosquitto/log:rw \
+-v $(pwd)/mosquitto/config/mosquitto.conf:/mosquitto/config/mosquitto.conf:ro \
+-v $(pwd)/mosquitto/data:/mosquitto/data:rw \
+-v $(pwd)/mosquitto/log:/mosquitto/log:rw \
 -u 996:995 \
 -p 1883:1883 \
 -p 9001:9001 \
