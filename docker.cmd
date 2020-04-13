916deeda360c397d8c62ac66069db3d58e191a36
pasciicode <769830+pasciicode@users.noreply.github.com>
2020-04-13 19:20:13 +0100

    Update docker.cmd

--- docker.cmd
+++ docker.cmd
@@ -1,7 +1,8 @@
 docker run -itd --name=mosquitto \
 --restart=always \
--v $(pwd)/mosquitto:/mosquitto:rw \
--u 996:995 \
+-v $(pwd)/mosquitto/config:/mosquitto/config:ro \
+-v $(pwd)/mosquitto/log:/mosquitto/log \
+-v $(pwd)/mosquitto/data:/mosquitto/data \
 -p 1883:1883 \
 -p 9001:9001 \
 -e TZ=Europe/Amsterdam \
