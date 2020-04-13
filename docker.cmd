d4ae2e40e5eefc5378bf67e7cee81c13145ba24f
pasciicode <769830+pasciicode@users.noreply.github.com>
2020-04-13 21:44:22 +0100

    Delete docker.cmd

--- docker.cmd
+++
@@ -1,9 +0,0 @@
-docker run -itd --name=mosquitto \
---restart=always \
--v $(pwd)/mosquitto/config:/mosquitto/config:ro \
--v $(pwd)/mosquitto/log:/mosquitto/log \
--v $(pwd)/mosquitto/data:/mosquitto/data \
--p 1883:1883 \
--p 9001:9001 \
--e TZ=Europe/Amsterdam \
-eclipse-mosquitto
